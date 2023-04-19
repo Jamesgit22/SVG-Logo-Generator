8d6b17811e47506f88a093c7b75136b6aeea32d0
Jamesgit22 <jschroeder97@icloud.com>
2023-04-19 16:19:15 -0500

    Added class to create new shape objects

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 // Questions to use to select SVG choices.
 const questions = [
-    'Please enter a name for your logo (Cannot exceed three characters).',
-    'Please eneter a color for the text.',
-    'Please choose a shape for your logo.',
-    'Please choose a color for the background.'
+  "Please enter a name for your logo (Cannot exceed three characters).",
+  "Please eneter a color for the text.",
+  "Please choose a shape for your logo.",
+  "Please choose a color for the background.",
 ];
 
 const init = () => {
-    inquirer
-  .prompt([
-    {
-        type: 'input',
+  inquirer
+    .prompt([
+      {
+        type: "input",
         message: questions[0],
-        name: 'logoText'
-    },
-    {
-        type: 'input',
+        name: "logoText",
+      },
+      {
+        type: "input",
         message: questions[1],
-        name: 'logoTextColor'
-    },
-    {
-        type: 'list',
+        name: "logoTextColor",
+      },
+      {
+        type: "list",
         message: questions[2],
-        choices: ['Square', 'Circle', 'Triangle'],
-        name: 'logoShape'
-    },
-    {
-        type: 'input',
+        choices: ["Square", "Circle", "Triangle"],
+        name: "logoShape",
+      },
+      {
+        type: "input",
         message: questions[3],
-        name: 'logoShapeColor'
-    }
-    
-  ])
-  .then((answers) => {
-
-    // Use user feedback for... whatever!!
-    console.log(answers);
-  })
-  .catch((error) => {
-    if (error) throw error;
-  });
-}
+        name: "logoShapeColor",
+      },
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+      console.log(answers);
+    })
+    .catch((error) => {
+      if (error) throw error;
+    });
+};
 
 init();
